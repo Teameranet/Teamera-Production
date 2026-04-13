2507ebaeff764a51bf462027269637a3dca1019d
Teameranet <teamera.net@gmail.com>
2026-04-13 17:59:56 +0530

    Commit
    - When User click "My Workspace" from the navbar dropdown (both desktop and mobile), it will  navigate to the dedicated Workspace page instead of opening a modal.
    
    - Display the Simple message  " Page is Comming Soon" on workspace page

--- frontend/pages/Workspace.jsx
+++ frontend/pages/Workspace.jsx
@@ -1,1 +1,11 @@
+import './Workspace.css';
 
+function Workspace() {
+  return (
+    <div className="workspace-coming-soon">
+      <p>Page is Coming Soon</p>
+    </div>
+  );
+}
+
+export default Workspace;
